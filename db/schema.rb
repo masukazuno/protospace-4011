b8987d5c6811640ac4a2d06ed1c0123549cfce6b
masukazuno <wnsxa43351@gmail.com>
2024-01-23 22:00:14 +0900

    デプロイ
    デプロイ

--- db/schema.rb
+++ db/schema.rb
@@ -59,11 +59,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_21_111413) do
     t.index ["user_id"], name: "index_prototypes_on_user_id"
   end
 
-  create_table "tweets", charset: "utf8", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
